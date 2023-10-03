994e70732f4dacd5287fe3003c5418264cb4af75
Naeem-ur-rahman <naeemur1001@gmail.com>
2023-10-03 19:35:32 +0500

    Group chat server created. The server send messages to all

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const PORT = 3000;
 const server = createServer(app);
 app.use(express.static(join(__dirname, 'public')))
 
+const io = new Server(server);
+
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'public', 'home.html'))
 })
@@ -16,6 +18,24 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.send('<h1> Web Socket Learing </h1>')
 })
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected', socket.id);
+//     socket.on('chat message', (msg) => {
+//         console.log('Message : ', msg)
+//         socket.broadcast.emit('hi');
+//     })
+//     socket.on('disconnect', () => {
+//         console.log('user disconnected', socket.id);
+//     });
+// })
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        io.emit('user', socket.id);
+    });
+});
 
 server.listen(PORT, () => {
     console.log(`server running at http://localhost:${PORT}`)
